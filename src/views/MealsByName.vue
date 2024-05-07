87e35623fa79473fe2bf7b2679d2e73c259b7a5b
EmreTopal <=>
2024-05-07 20:42:47 +0300

    publish the project v1

---
+++ src/views/MealsByName.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="p-8">
+    <input 
+    type="text"
+    v-model="keyword"
+    class="rounded border-2 border-gray-200 w-full"
+    placeholder="Search for Meals" 
+    @change="searchMeals">
+  </div>
+
+  <Meals :meals="meals" />
+</template>
+
+<script setup>
+import { computed } from '@vue/reactivity';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import  store from '../store';
+import Meals from '../components/Meals.vue';
+
+
+const route = useRoute();
+const keyword = ref('');
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch('searchMeals', keyword.value)
+}
+
+
+onMounted(() => {
+  keyword.value = route.params.name;
+
+  if (keyword.value) {
+    searchMeals();
+  }
+})
+</script>
